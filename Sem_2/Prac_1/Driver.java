5d5c7b70eefb361e15f71314947e5ccdd00997c4
Jarad Leo Humphry <hmpjar001@sl-dual-172.cs.uct.ac.za>
2016-08-04 17:30:31 +0200

    code done :)

--- Sem_2/Prac_1/Driver.java
+++ Sem_2/Prac_1/Driver.java
@@ -14,119 +14,118 @@ public class Driver
 		String sort = args[0];
 		System.out.println("args?");
 		//if (sort.equals("Mergesort"))
-			//{
-			System.out.println("a");
-			//Integer.parseInt()
+		//{
+		System.out.println("a");
+		//Integer.parseInt()
+		int ArraySize;
+		//int NumberOfThreads = 8;
+		//int Threshold = 1 + (ArraySize/Runtime.getRuntime().availableProcessors());
+		//System.out.println("Number of Processors: " + Runtime.getRuntime().availableProcessors());	
+		int min = Integer.parseInt(args[1]);
+		int max = Integer.parseInt(args[2]);
+		int increment = Integer.parseInt(args[3]);
+		//int[] Arr1 = new int[ArraySize];
+		//int[] Arr2 = new int[ArraySize];
+		long[] Arr3 = new long[5];
+		long[] Arr4 = new long[5];
 
-			int ArraySize;
-			//int NumberOfThreads = 8;
-			//int Threshold = 1 + (ArraySize/Runtime.getRuntime().availableProcessors());
-			//System.out.println("Number of Processors: " + Runtime.getRuntime().availableProcessors());	
+		//System.out.println("b");
 
-			int min = Integer.parseInt(args[1]);
-			int max = Integer.parseInt(args[2]);
-			int increment = Integer.parseInt(args[3]);
-			//int[] Arr1 = new int[ArraySize];
-			//int[] Arr2 = new int[ArraySize];
-			long[] Arr3 = new long[5];
-			long[] Arr4 = new long[5];
+		for (int p = min; p < max; p=p+increment)
+			{
+			//System.out.println("c");
+			ArraySize = p;
+			int[] Arr1 = new int[ArraySize];
+			int[] Arr2 = new int[ArraySize];
+			long minTime = 999999999;
+			int Threads = 2;
+			long sortTime = 999999999;
+			
+			for (int i=0;i<Arr1.length;i++)
+				{		
+				Arr1[i] = randomNum();
+				//System.out.println("" + i + ": " + Arr1[i]);
+				}
 
-			//System.out.println("b");
-
-			for (int p = min; p < max; p=p+increment)
+			for (int a=2; a < 10000; a=a*2)
 				{
-				//System.out.println("c");
-				ArraySize = p;
-				int[] Arr1 = new int[ArraySize];
-				int[] Arr2 = new int[ArraySize];
+				//System.out.println("Testing for " + a + " Threads:");
+				for (int b = 0; b < 5; b++)
+					{
+				
 
-					for (int i=0;i<Arr1.length;i++)
-						    {		
-							Arr1[i] = randomNum();
-							//System.out.println("" + i + ": " + Arr1[i]);
-						    }
+					//System.out.println("The initial list is:");
+	
+					System.arraycopy(Arr1, 0, Arr2, 0, ArraySize);
+					System.gc();
+					tick();
+					Arrays.sort(Arr2);
+					long time = toc();
+					sortTime = time;
+					//Arr3[b] = time;
 
-					for (int a=2; a < 18; a=a+2)
+					//System.out.println("Time taken in series: " + time);
+					System.arraycopy(Arr1, 0, Arr2, 0, ArraySize);
+					if (sort.equals("Mergesort"))
 						{
-						System.out.println("Testing for " + a + " Processors:");
-						for (int b = 0; b < 5; b++)
-							{
-				
-
-							//System.out.println("The initial list is:");
-
-				
-		
-							System.arraycopy(Arr1, 0, Arr2, 0, ArraySize);
-
-		
-							tick();
-							Arrays.sort(Arr2);
-							long time = toc();
-							Arr3[b] = time;
-
-							//System.out.println("Time taken in series: " + time);
-
-							System.arraycopy(Arr1, 0, Arr2, 0, ArraySize);
-
-							if (sort.equals("Mergesort"))
-								{
-								ParallelMergeSort PMS = new ParallelMergeSort(Arr2, 0, Arr2.length, 1 + (Arr1.length)/a);		
-								tick();
-								ForkJoinPool.commonPool().invoke(PMS);
-								
-								
-								time = toc();
-								Arr4[b] = time;
-								}
-							else if (sort.equals("Quicksort"))
-								{
-								QuickSort QS = new QuickSort(Arr2, 0, Arr2.length - 1);
-								tick();
-								ForkJoinPool.commonPool().invoke(QS);
-								time = toc();
-								Arr4[b] = time;
-								}
+						ParallelMergeSort PMS = new ParallelMergeSort(Arr2, 0, Arr2.length, 1 + (Arr1.length)/a);							
+						System.gc();
+						tick();
+						ForkJoinPool.commonPool().invoke(PMS);
 							
-
-							System.out.println("Time taken in parralel: " + time);
-
-							System.out.println("");
-							System.out.println("The sorted version:");
-							for (int k=0;k<Arr2.length;k++)
-								{
-								System.out.println("" + k + ": " + Arr2[k]);
-								}
-		
-							/*System.out.println("The sorted version:");
-							for (int p=0;p<Arr1.length;p++)
-								{
-								System.out.println("" + p + ": " + Arr1[p]);
-								}*/
-							}
-						System.out.println("Using " + a + " processes:");
-						long l = Arr3[0];
-						long m = Arr4[0];
-
-						for (int c = 1; c < 5; c ++)
+							
+						time = toc();
+						//Arr4[b] = time;
+						if (minTime == 999999999)
 							{
-							if (Arr3[c] < l)
-								{
-								l = Arr3[c];
-								}
-							if (Arr4[c] < m)
-								{
-								m = Arr4[c];
-								}
+							minTime = time;
+							Threads = a;
 							}
-						Arrays.sort(Arr3);
-						Arrays.sort(Arr4);
-						System.out.println("The shortest serialsort time is: " + Arr3[0]);
-						System.out.println("The shortest parasort time is: " + Arr4[0]); 
+						if (time < minTime)
+							{
+							minTime = time;
+							Threads = a;
+							}
+						}
+							
+							
+									//System.arraycopy(Arr1, 0, Arr2, 0, ArraySize);
+					if (sort.equals("Quicksort"))
+						{
+						QuickSort QS = new QuickSort(Arr2, 0, Arr2.length - 1, 1 + (Arr1.length)/a);
+						System.gc();
+						tick();
+						ForkJoinPool.commonPool().invoke(QS);
+						time = toc();	
+						//Arr4[b] = time;
+						//System.out.println("minTime: " + minTime + " time: " + time);
+						if (minTime == 999999999)
+							{
+							minTime = time;
+							Threads = a;
+							//System.out.println("Threads set to: " + Threads);
+							}
+						if (time < minTime)
+							{
+							minTime = time;
+							Threads = a;
+							//System.out.println("minTime set to: " + minTime);
+							}
 						}
 					}
+
+					
+							//Arrays.sort(Arr3);
+							//Arrays.sort(Arr4);
+					
+						
+							//System.out.println("The shortest serialsort time is: " + Arr3[0]);
+							//System.out.println("The shortest parasort time is: " + Arr4[0]); 
+					
 				}
-			//}
+			System.out.println(ArraySize + "	" + Threads + "	" + minTime + "	" + (double)sortTime/minTime);
+			}
+		}	
 
 	public static int randomNum ()
 		{
@@ -137,11 +136,11 @@ public class Driver
 
 	private static void tick()
 		{
-		startTime = System.currentTimeMillis();
+		startTime = System.nanoTime();
 		}
 
 	private static long toc()
 		{
-		return (System.currentTimeMillis() - startTime) /*/ 1000.0f*/; 
+		return (System.nanoTime() - startTime) /*/ 1000.0f*/; 
 		}
 	}
